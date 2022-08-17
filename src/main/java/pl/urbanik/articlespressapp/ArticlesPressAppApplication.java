734fd580aa68a0a00bcf299e911269c55992a719
Kamil <Kamil.Urbanik@o2.pl>
2022-08-17 10:01:56 +0200

    #0001 - initial commit

---
+++ src/main/java/pl/urbanik/articlespressapp/ArticlesPressAppApplication.java
@@ -0,0 +1,13 @@
+package pl.urbanik.articlespressapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesPressAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesPressAppApplication.class, args);
+	}
+
+}
